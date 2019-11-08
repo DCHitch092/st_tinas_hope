bb14c4f013dfc6974cdc186ff2c72fbbb733d768
Richard <fahitch+github@gmail.com>
2019-11-08 22:34:10 +0000

    adds save function to vets class

--- models/vet.rb
+++ models/vet.rb
@@ -13,4 +13,16 @@ class Vet
     @profile_image = options['profile_image']
   end
 
+  def save()
+    sql = "INSERT INTO vets
+    ( name, role, fav_colour, profile_image )
+    VALUES
+    ( $1, $2, $3, $4 )
+    RETURNING id;"
+    values = [ @name, @role, @fav_colour, @profile_image ]
+    result = SqlRunner.run( sql, values)[0]
+    @id = result['id'].to_i
+
+  end
+
 end

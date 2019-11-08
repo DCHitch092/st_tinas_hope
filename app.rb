16141eeaf3827325e43170cf158df4e2c0d2d1cf
Richard <fahitch+github@gmail.com>
2019-11-08 23:05:26 +0000

    adds required files to app controller

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
+# required controller files
+require_relative('./controllers/vets_controller')
+
+# required model files
 require_relative('./models/vet')
 also_reload('./models/*')
 

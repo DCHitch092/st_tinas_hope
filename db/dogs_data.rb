785a2d464486a2256881b960a9a3494a2c03b9b6
Richard <fahitch+github@gmail.com>
2019-11-09 23:57:04 +0000

    updates dogs data for correct filenames, turns publicinto hidden folder to keep images hidden

--- db/dogs_data.rb
+++ db/dogs_data.rb
@@ -13,13 +13,13 @@ vet1 = Vet.new(
   { 'name' => 'Nurse Elmo',
     'role' => 'Nurse',
     'fav_colour' => 'rainbow',
-    'profile_image' => 'nurse_elmo.png'})
+    'profile_image' => 'nurse-elmo.png'})
 
 vet2 = Vet.new(
   { 'name' => 'Nurse ChumChi',
     'role' => 'Nurse',
     'fav_colour' => 'orange',
-    'profile_image' => 'nurse_chumchi.png'})
+    'profile_image' => 'nurse-chumchi.png'})
 
 vet1.save()
 vet2.save()

b8238b7e46c00a6c3feb17420cd7b74a106f1f67
Richard <fahitch+github@gmail.com>
2019-11-09 22:28:38 +0000

    adds vet_assign table to database in sql file

--- db/vet_project.sql
+++ db/vet_project.sql
@@ -1,29 +1,36 @@
+DROP TABLE vet_assign;
 DROP TABLE vets;
 DROP TABLE animals;
 DROP TABLE humans;
 
 CREATE TABLE vets(
   id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255),
-  role VARCHAR(255),
-  fav_colour VARCHAR(255),
+  name          VARCHAR(255),
+  role          VARCHAR(255),
+  fav_colour    VARCHAR(255),
   profile_image VARCHAR(255)
 );
 
 CREATE TABLE animals(
-  id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255),
+  id SERIAL4    PRIMARY KEY,
+  name          VARCHAR(255),
   date_of_birth VARCHAR(255),
-  type VARCHAR(255),
-  fav_colour VARCHAR(255),
-  age INT4
+  type          VARCHAR(255),
+  fav_colour    VARCHAR(255),
+  age           INT4
 );
 
 CREATE TABLE humans(
   id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  address TEXT,
-  phone VARCHAR(255),
+  name       VARCHAR(255),
+  email      VARCHAR(255),
+  address    TEXT,
+  phone      VARCHAR(255),
   fav_colour VARCHAR(255)
 );
+
+CREATE TABLE vet_assign(
+  id        SERIAL4 PRIMARY KEY,
+  vet_id    INT4 REFERENCES vets(id),
+  animal_id INT4 REFERENCES animals(id)
+)

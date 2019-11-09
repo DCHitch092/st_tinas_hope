88f2f8773367a15babc5140849ef0d9ecc68ca50
Richard <fahitch+github@gmail.com>
2019-11-09 07:59:00 +0000

    adds animals table to sql file

--- db/vet_project.sql
+++ db/vet_project.sql
@@ -1,4 +1,5 @@
 DROP TABLE vets;
+DROP TABLE animals
 
 CREATE TABLE vets(
   id SERIAL4 PRIMARY KEY,
@@ -6,4 +7,13 @@ CREATE TABLE vets(
   role VARCHAR(255),
   fav_colour VARCHAR(255),
   profile_image VARCHAR(255)
+);
+
+CREATE TABLE animals(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  date_of_birth VARCHAR(255),
+  type VARCHAR(255),
+  fav_colour VARCHAR(255),
+  age INT4
 );

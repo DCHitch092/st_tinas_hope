c0c554bfda63317ea87ca879ae586f9a94bb64e9
Richard <fahitch+github@gmail.com>
2019-11-09 19:35:03 +0000

    adds relative requirements to app.rb for humans controller and model

--- app.rb
+++ app.rb
@@ -5,10 +5,12 @@ require('pry')
 # required controller files
 require_relative('./controllers/vets_controller')
 require_relative('./controllers/animals_controller')
+require_relative('./controllers/humans_controller')
 
 # required model files
 require_relative('./models/vet')
 require_relative('./models/animal')
+require_relative('./models/human')
 also_reload('./models/*')
 
 get '/' do

7c19ed7cf029cb2d99dfeb69b9c9bf6120915532
Richard <fahitch+github@gmail.com>
2019-11-11 12:39:21 +0000

    adds notes table to database sql file

--- db/vet_project.sql
+++ db/vet_project.sql
@@ -3,9 +3,10 @@
 DROP TABLE animals;
 DROP TABLE vets;
 DROP TABLE humans;
+DROP TABLE notes;
 
 CREATE TABLE vets(
-  id SERIAL4 PRIMARY KEY,
+  id            SERIAL4 PRIMARY KEY,
   name          VARCHAR(255),
   role          VARCHAR(255),
   fav_colour    VARCHAR(255),
@@ -13,7 +14,7 @@ CREATE TABLE vets(
 );
 
 CREATE TABLE humans(
-  id SERIAL4 PRIMARY KEY,
+  id         SERIAL4 PRIMARY KEY,
   name       VARCHAR(255),
   email      VARCHAR(255),
   address    TEXT,
@@ -22,7 +23,7 @@ CREATE TABLE humans(
 );
 
 CREATE TABLE animals(
-  id SERIAL4    PRIMARY KEY,
+  id            SERIAL4    PRIMARY KEY,
   name          VARCHAR(255),
   date_of_birth VARCHAR(255),
   type          VARCHAR(255),
@@ -30,6 +31,14 @@ CREATE TABLE animals(
   human_id      INT4 REFERENCES humans(id),
   vet_id        INT4 REFERENCES vets(id),
   age           INT4
+);
+
+CREATE TABLE notes(
+  id        SERIAL4 PRIMARY KEY,
+  animal_id INT4 REFERENCES animals(id),
+  vet_id    INT4,
+  body      TEXT,
+  date      VARCHAR(255)
 );
 
 -- CREATE TABLE vet_assign(
